01c4da268fbdebfcfd802f2346810fd7165652b5
owner <owner@owners-MacBook-Air.local>
2026-09-11 12:25:38 +0200

    Update MyComponent paragraph text via props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MyComponent />
+      <MyComponent title="Hello from MyComponent!" text="hello thi is my first component made in react." />
     </div>
   );
 }
